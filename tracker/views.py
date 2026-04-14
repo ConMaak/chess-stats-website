c329081fd6ef78179941f9a9d19f9efe86c849d3
Connor Maake <connormaake@gmail.com>
2026-04-13 22:02:17 -0400

    Created basic frontend player dashboard with general player stats

--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Player
 
-def home_view(request):
+def home_view(request): 
+    if request.method == "POST":
+        username = request.POST.get("username", "").strip().lower()
+
+        if username:
+            return redirect("player_dashboard", username=username)
+
     return render(request, "tracker/home.html")
 
 def player_dashboard_view(request, username):
+    player = get_object_or_404(Player, username_normalized=username)
+    total_games = player.games.count()
     context = {
-        "username": username,
+        "player":player,
+        "total_games": total_games
     }
     return render(request, "tracker/player_dashboard.html", context)
