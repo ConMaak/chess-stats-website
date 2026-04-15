34400e9e8a916f8322f6bcee8b9166dbeb04c19e
Connor Maake <connormaake@gmail.com>
2026-04-14 21:06:02 -0400

    Added recent games table to frontend

--- tracker/views.py
+++ tracker/views.py
@@ -13,8 +13,10 @@ def home_view(request):
 def player_dashboard_view(request, username):
     player = get_object_or_404(Player, username_normalized=username)
     total_games = player.games.count()
+    recent_games = player.games.order_by("-end_time")[:10]
     context = {
         "player":player,
-        "total_games": total_games
+        "total_games": total_games,
+        "recent_games": recent_games,
     }
     return render(request, "tracker/player_dashboard.html", context)
